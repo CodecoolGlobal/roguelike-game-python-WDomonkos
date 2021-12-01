97449e19e64565649bf53f154cba8496f9047bdc
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2021-12-01 13:04:22 +0100

    add three different room

--- engine.py
+++ engine.py
@@ -2,45 +2,123 @@ from typing import Counter
 from math import floor
 
 from main import PLAYER_ICON, PLAYER_START_X, PLAYER_START_Y
+import copy
+
+WALL = "#"
+EMPTY = " "
+RIGHT_GATE = '\033[96m'+">"+'\033[00m'
+LEFT_GATE = '\033[96m'+"<"+'\033[00m'
+
+def create_board(width, height, border_width = 1):
+    matrix = []
+    for h in range(height):
+        inner_list = []
+        for w in range(width):
+            inner_list.append(WALL)
+        matrix.append(inner_list)
+
+    if border_width in range(1,min(height, width)//2 + 1):
+        for inner_h in range(border_width,height-border_width):
+            for inner_w in range(border_width,width-border_width):
+                matrix[inner_h][inner_w] = EMPTY
+    
+    return matrix
+
+# def create_board(width, height):
+#     '''
+#     Creates a new game board based on input parameters.
+
+#     Args:
+#     int: The width of the board
+#     int: The height of the board
+
+#     Returns:
+#     list: Game board
+#     '''
+   
+#     board = []
+#     for row in range(height):
+#         column = []
+#         for col in range(width):
+#             if row == 0 or row == height -1:    # northern and southern WALL
+#                 column.append(WALL)
+#             elif height % 2 == 1 and floor(height / 2) == row:  # GATE if height is odd
+#                 if col == 0 or col == width -1:
+#                     column.append(RIGHT_GATE)
+#                 else:
+#                     column.append(EMPTY)
+#             elif height % 2 == 0 and (height / 2) -1 == row or (height / 2) == row:     # GATE if height is even
+#                 if col == 0 or col == width -1:
+#                     column.append(LEFT_GATE)
+#                 else:
+#                     column.append(EMPTY)
+#             else:                                   # every other row
+#                 if col == 0 or col == width -1:
+#                     column.append(WALL)
+#                 else:
+#                     column.append(EMPTY)
+#         board.append(column)
+#     return board
 
 
-def create_board(width, height):
-    '''
-    Creates a new game board based on input parameters.
+def board1(orig_board):
+    new_board = copy.deepcopy(orig_board)
 
-    Args:
-    int: The width of the board
-    int: The height of the board
+    new_board[len(new_board)//2][-1] = RIGHT_GATE
+    new_board[len(new_board)//2-1][-1] = RIGHT_GATE
 
-    Returns:
-    list: Game board
-    '''
-    wall = "#"
-    empty = " "
-    gate = "¤"
-    board = []
-    for row in range(height):
-        column = []
-        for col in range(width):
-            if row == 0 or row == height -1:    # northern and southern wall
-                column.append(wall)
-            elif height % 2 == 1 and floor(height / 2) == row:  # gate if height is odd
-                if col == 0 or col == width -1:
-                    column.append(gate)
-                else:
-                    column.append(empty)
-            elif height % 2 == 0 and (height / 2) -1 == row or (height / 2) == row:     # gate if height is even
-                if col == 0 or col == width -1:
-                    column.append(gate)
-                else:
-                    column.append(empty)
-            else:                                   # every other row
-                if col == 0 or col == width -1:
-                    column.append(wall)
-                else:
-                    column.append(empty)
-        board.append(column)
-    return board
+    for i in range(9):
+        new_board[i][10] = WALL
+    for i in range(13):
+        new_board[-i][16] = WALL
+
+    new_board[2][-(len(orig_board[0])//2+2)] = "\033[92m"+"H"+"\033[00m"
+    new_board[2][-(len(orig_board[0])//2+1)] = "\033[92m"+"O"+"\033[00m"
+    new_board[2][-(len(orig_board[0])//2+0)] = "\033[92m"+"M"+"\033[00m"
+    new_board[2][-(len(orig_board[0])//2-1)] = "\033[92m"+"E"+"\033[00m"
+
+    return new_board
+
+
+def board2(orig_board):
+    new_board = copy.deepcopy(orig_board)
+
+    new_board[len(new_board)//2][-1] = RIGHT_GATE
+    new_board[len(new_board)//2-1][-1] = RIGHT_GATE
+
+    new_board[len(new_board)//2][0] = LEFT_GATE
+    new_board[len(new_board)//2-1][0] = LEFT_GATE
+
+    for i in range(18):
+        new_board[13][i] = WALL
+    for i in range(13):
+        new_board[7][-i] = WALL
+
+    new_board[2][-(len(orig_board[0])//2+3)] = "\033[92m"+"S"+"\033[00m"
+    new_board[2][-(len(orig_board[0])//2+2)] = "\033[92m"+"T"+"\033[00m"
+    new_board[2][-(len(orig_board[0])//2+1)] = "\033[92m"+"R"+"\033[00m"
+    new_board[2][-(len(orig_board[0])//2-0)] = "\033[92m"+"E"+"\033[00m"
+    new_board[2][-(len(orig_board[0])//2-1)] = "\033[92m"+"E"+"\033[00m"
+    new_board[2][-(len(orig_board[0])//2-2)] = "\033[92m"+"T"+"\033[00m"
+
+    return new_board
+
+
+def board3(orig_board):
+    new_board = copy.deepcopy(orig_board)
+
+    new_board[len(new_board)//2][0] = LEFT_GATE
+    new_board[len(new_board)//2-1][0] = LEFT_GATE
+
+    for i in range(len(orig_board)-5):
+        new_board[i][-8] = WALL
+
+    new_board[2][-5] = "\033[92m"+"B"+"\033[00m"
+    new_board[2][-4] = "\033[92m"+"A"+"\033[00m"
+    new_board[2][-3] = "\033[92m"+"R"+"\033[00m"
+
+    return new_board
+
             
 
 def put_player_on_board(board, player):
@@ -68,6 +146,18 @@ def player_movement(board, player, key):
         player["position_x"] += keys[key]
     elif key == "w" or key == "s":
         player["position_y"] += keys[key]
-    if board[player["position_y"]][player["position_x"]] == "#":
+    if board[player["position_y"]][player["position_x"]] == WALL:
         return player_original
+
+    if board[player["position_y"]][player["position_x"]] == RIGHT_GATE:
+        player["current_room"] += 1
+        player["position_y"] = len(board)//2
+        player["position_x"] = 1
+
+    if board[player["position_y"]][player["position_x"]] == LEFT_GATE:
+        player["current_room"] -= 1
+        player["position_y"] = len(board)//2
+        player["position_x"] = -2
+
+
     return player
--- main.py
+++ main.py
@@ -18,25 +18,35 @@ def create_player():
     Returns:
     dictionary
     '''
-    player = { "icon": PLAYER_ICON, "position_x": PLAYER_START_X, "position_y": PLAYER_START_Y, "wallet": 0, "lives": 5}
+    player = { "icon": PLAYER_ICON, "position_x": PLAYER_START_X, "position_y": PLAYER_START_Y, "wallet": 0, "lives": 5, "current_room": 1}
     return player
 
 
 def main():
     player = create_player()
     board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
+    board1 = engine.board1(board)
+    board2 = engine.board2(board)
+    board3 = engine.board3(board)
 
     util.clear_screen()
     is_running = True
     while is_running:
-        engine.put_player_on_board(board, player)
-        ui.display_board(board)
+        if player["current_room"] == 1:
+            current_room = board1
+        elif player["current_room"] == 2:
+            current_room = board2
+        elif player["current_room"] == 3:
+            current_room = board3
+
+        engine.put_player_on_board(current_room, player)
+        ui.display_board(current_room)
 
         key = util.key_pressed()
         if key == 'q':
             is_running = False
         else:
-            player = engine.player_movement(board, player, key)
+            player = engine.player_movement(current_room, player, key)
         util.clear_screen()
 
 
