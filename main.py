df62b8fca6f25a123ae606fffe08ca20d43736d9
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2021-12-01 15:12:06 +0100

    add more characters

--- main.py
+++ main.py
@@ -23,9 +23,11 @@ def create_player():
     Returns:
     dictionary
     '''
-    player = { "icon": PLAYER_ICON, "position_x": PLAYER_START_X, "position_y": PLAYER_START_Y, "wallet": 0, "lives": 5, "current_room": 1}
+    player = { "icon": PLAYER_ICON, "position_x": PLAYER_START_X, "position_y": PLAYER_START_Y, "wallet": 0, "lives": 5, "current_room": 1, "key": 0}
     return player
 
+# def create_key():
+#     key = {"icon": "~", "position_x": randint(1, BOARD_WIDTH -1), "position_y": randint(1, BOARD_HEIGHT-1)}
 
 def create_passer_by():
     passer_by = {"icon": "¤", "wallet": randint(0, 6), "position_x": randint(1, BOARD_WIDTH -1), "position_y": randint(1, BOARD_HEIGHT-1)}
@@ -33,7 +35,7 @@ def create_passer_by():
 
 
 def create_barkeeper():
-    barkeeper = {"icon": "$", "drinks": 1, "wallet": 2}
+    barkeeper = {"icon": "$", "drinks": 1, "wallet": 2, "position_x": -5, "position_y": randint(1, BOARD_HEIGHT-1)}
     return barkeeper
 
 
@@ -43,13 +45,13 @@ def create_cop():
 
 
 def create_boss():
-    boss_jozsi = {"icon": "<>", "drinks": 1, "lives": 3}
+    boss_jozsi = {"icon": "§", "drinks": 1, "lives": 3, "position_x": 15, "position_y": 10}
     return boss_jozsi
 
 
 
-def wife():
-    wife = {"icon": "&", "wallet": 0, "position_x": randint(1, BOARD_WIDTH -1), "position_y": randint(1, BOARD_HEIGHT-1)}
+def create_wife():
+    wife = {"icon": "\033[93m"+"&"+"\033[00m", "wallet": 0, "position_x": randint(1, BOARD_WIDTH -1), "position_y": randint(1, BOARD_HEIGHT-1)}
     return wife
 
 
@@ -91,7 +93,13 @@ def choosing_difficulty():
 def main():
     player = create_player()
     passer_by = create_passer_by()
-    cop = create_cop()
+    cop1 = create_cop()
+    cop2 = create_cop()
+    cop3 = create_cop()
+    wife = create_wife()
+    barkeeper = create_barkeeper()
+    boss = create_boss()
+    # key = create_key()
     # board = engine.create_map()
     # board_for_print = util.map_indexing(board)
     board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
@@ -102,16 +110,25 @@ def main():
     util.clear_screen()
     is_running = True
     while is_running:
+        
+
         if player["current_room"] == 1:
             current_room = board1
+            engine.put_player_on_board(current_room, wife)
+            #engine.put_player_on_board(current_room, key)
+
         elif player["current_room"] == 2:
             current_room = board2
+            engine.put_player_on_board(current_room, cop1)
+            engine.put_player_on_board(current_room, cop2)
+            engine.put_player_on_board(current_room, cop3)
+            engine.put_player_on_board(current_room, passer_by)
         elif player["current_room"] == 3:
             current_room = board3
+            engine.put_player_on_board(current_room, barkeeper)
+            #engine.put_player_on_board(current_room, boss)
 
         engine.put_player_on_board(current_room, player)
-        engine.put_player_on_board(current_room, passer_by)
-        engine.put_player_on_board(current_room, cop)
         ui.display_board(current_room)
 
         key = util.key_pressed()
@@ -120,8 +137,13 @@ def main():
         else:
             # player = engine.player_movement(board_for_print, player, key)
 
-            cop = engine.player_movement(current_room, cop, key =choice(["s", "w"]))
-            passer_by = engine.player_movement(current_room, passer_by, key =choice(["a", "s", "d", "w"]))
+            cop1 = engine.player_movement(current_room, cop1, key=choice(["s", "w"]))
+            cop2 = engine.player_movement(current_room, cop2, key=choice(["s", "w"]))
+            cop3 = engine.player_movement(current_room, cop3, key=choice(["s", "w"]))
+            barkeeper = engine.player_movement(current_room, barkeeper, key=choice(["s", "w"]))
+            passer_by = engine.player_movement(current_room, passer_by, key=choice(["a", "s", "d", "w"]))
+            boss = engine.player_movement(current_room, boss, key=choice(["a", "s", "d", "w"]))
+            wife = engine.player_movement(current_room, wife, key=choice(["a", "s", "d", "w"]))
             player = engine.player_movement(current_room, player, key)
         util.clear_screen()
 
