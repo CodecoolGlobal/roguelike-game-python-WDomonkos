23844faf102d23cf42e58a4499154939a094e6c6
oliviakovacs <oliviakovacs@github.com>
2021-12-01 11:10:04 +0100

    Creating characters first step

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import util
 import engine
 import ui
+from random import randint
 
 PLAYER_ICON = '@'
 PLAYER_START_X = 3
@@ -20,6 +21,26 @@ def create_player():
     '''
     player = { "icon": PLAYER_ICON, "position_x": PLAYER_START_X, "position_y": PLAYER_START_Y, "wallet": 0, "lives": 5}
     return player
+
+
+def create_passer_by():
+    passer_by = {"icon": "¤", "wallet": randint(0, 6)}
+    return passer_by
+
+
+def create_barkeeper():
+    barkeeper = {"icon": "$", "drinks": 1, "wallet": 2}
+    return barkeeper
+
+
+def create_cop():
+    cop = {"icon": "%", "weapon": 0}
+    return cop
+
+
+def create_boss():
+    boss_jozsi = {"icon": "<>", "drinks": 1, "lives": 3}
+    return boss_jozsi
 
 
 def main():
